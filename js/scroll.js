c26c9ce879c92048fa7da7069164e6d0785512f1
Rafael_santospg <rafael_santospg@yahoo.com.br>
2023-07-22 03:06:12 -0300

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,33 @@
+//ANIMATION FOR EACH SECTION WHILE SCROLLING DOWN THE PAGE
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    //console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
